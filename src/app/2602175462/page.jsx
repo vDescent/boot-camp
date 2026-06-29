91cc7bd237fbb740775495a808f291bf164daa8b
vdescent <timotiusryjaska@gmail.com>
2026-06-29 14:08:39 +0700

    feat: add new page

---
+++ src/app/2602175462/page.jsx
@@ -0,0 +1,45 @@
+'use client'
+import React, {useState, useEffect, useRef, useMemo} from 'react';
+
+export default function MyPage(){
+    const myStateRightNow = ['chill', 'sleep', 'code', 'reading', 'learning', 'playing with my friends', 'go outside to enjoy world'];
+    const inputRef = useRef(null);
+    const [state, setState] = useState('chill');
+    const [count, setCount] = useState(0);
+    
+    const randomized = ()=>{
+        const randomIndex = Math.floor(Math.random() * myStateRightNow.length);
+        setState(myStateRightNow[randomIndex]);
+    }
+
+    function handleInputClick(){
+        inputRef.current.focus();
+    }
+
+    const slowFunc = (num)=>{
+        for(let i = 0;i<990000000;i++){}
+        return num*2;
+    }
+
+    const doubleNumUseMemo = useMemo(()=>{
+        return slowFunc(count);
+    }, [count])
+
+
+    return(
+        <div className='flex flex-col justify-center items-center min-h-screen min-w-screen'>
+            <h1>Timotius - 2602175462</h1>
+            <p>Student of Computer Science</p>
+            <h2>useRef</h2>
+            <input type="text" ref={inputRef} placeholder='Type something' />
+            <button onClick={handleInputClick}>Focus input</button>
+            <h2>useState without useMemo</h2>
+            {/* <button onClick={()=> setState(randomized)}>Generate State</button> */}
+            <button onClick={randomized}>Generate State</button>
+            <p>My state right now : <strong>{state}</strong></p>
+            <h2>useMemo</h2>
+            <button onClick={()=> setCount(count + 1)}>useMemo (if you click this usememo effect will be little bit lag and that's okay)</button>
+            <p>Usememo Effect: {doubleNumUseMemo}</p>
+        </div>
+    )
+}
