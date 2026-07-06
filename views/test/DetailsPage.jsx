9889543d54a9158ff80c6b3bac4474d796ff396a
vdescent <timotiusryjaska@gmail.com>
2026-07-06 17:01:57 +0700

    chore: add useref and usememo

--- views/test/DetailsPage.jsx
+++ views/test/DetailsPage.jsx
@@ -1,17 +1,8 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 
 export default function DetailsPage() {
-    // const response = await fetch("https://catfact.ninja/fact");
-    // // console.log(response);
-
-    // // error handling
-    // if(!response.ok){
-    //     throw new Error("Failed to fetch");
-    // }
-
-    // const data = await response.json();
-
+    const generateCount = useRef(0);
     const [fact,setFact] = useState('');
 
     useEffect(()=>{
@@ -23,12 +14,27 @@ export default function DetailsPage() {
         fetchFact();
     }, []);
 
+    async function onclickFetch(){
+        generateCount.current++;
+
+        const response = await fetch('https://catfact.ninja/fact');
+        const data = await response.json();
+        setFact(data.fact);
+    }
+
+    const totalWords = useMemo(()=>{
+        return fact.split(" ").length;
+    }, [fact]);
+
 
 
     return(
         <div style={{display:'flex', flexDirection:'column', justifyContent:"center", alignItems:'center', padding:'2rem'}}>
             <h2 style={{margin:'1rem', backgroundColor:'orange', padding:'16px', border:'solid 2px orange', borderRadius:'16px' }}>Random Fact For The Day: </h2>
-            <p style={{margin:'0', textAlign:'center', fontSize:'16px'}}>{fact}</p>
+            <h3 style={{margin:'0', textAlign:'center'}}>{fact}</h3>
+            <p>Words total : {totalWords}</p>
+            <button onClick={() =>onclickFetch()} style={{margin:'16px', backgroundColor:'lime', padding:'8px', borderRadius:'8px', cursor:'pointer'}}>Generate more</button>
+            <p>You have been generating for {generateCount.current} times</p>
         </div>
     )
 }
