fb18de99c3b6d8ec0b216c604484e2fb92a963e0
vdescent <timotiusryjaska@gmail.com>
2026-07-09 17:18:30 +0700

    feat: add firebase

--- src/app/assingment/09/2602175462/page.jsx
+++ src/app/assingment/09/2602175462/page.jsx
@@ -1,7 +1,27 @@
+"use client"
+
+
 import React from 'react'
+import { useEffect } from 'react'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from './firebase/Initialize'
 
 export default function page() {
+  useEffect(()=>{
+    const testFire = async ()=>{
+      try{
+        const snapshot = await getDocs(collection(db, "test"));
+
+        console.log(`Firestore tidak error, Jumlah dokumen:${snapshot.size}`);
+      } catch (error){
+        console.log(`Error`, error);
+      }
+    };
+    testFire();
+  }, []);
+
+
   return (
-    <div>page</div>
+    <div className='bg-amber-500 w-full h-full'>page</div>
   )
 }
