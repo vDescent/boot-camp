0905acfc9400cf6d8d2acf57ab3141a8923b1de6
vdescent <timotiusryjaska@gmail.com>
2026-07-17 19:57:24 +0700

    feat: add logout btn and functionality

--- src/app/assingment/10/2602175462/page.jsx
+++ src/app/assingment/10/2602175462/page.jsx
@@ -1,9 +1,29 @@
+"use client"
+
 import React from 'react'
 import { db } from './firebase/Init'
+import { auth } from './firebase/Init'
+import { signOut } from 'firebase/auth'
+import { useRouter } from 'next/navigation'
 
 export default function page() {
-  console.log(`db: `,db)
+  const router = useRouter();
+
+  const handleLogout = async()=>{
+    try{
+      const logOutUser = await signOut(auth);
+      console.log(`Sign out Success`);
+
+      router.replace(`/assingment/10/2602175462/login`);
+    } catch (error){
+      console.log(`Error ${error}`);
+    }
+  }
+
   return (
-    <div>page</div>
+    <div>
+      <h1>Main Page</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
   )
 }
