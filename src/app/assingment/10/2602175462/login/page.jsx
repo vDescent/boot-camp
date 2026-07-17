2432adf94efcda09ad7cb986dc949f9031ede5ab
vdescent <timotiusryjaska@gmail.com>
2026-07-17 14:38:19 +0700

    fix: fix register page

--- src/app/assingment/10/2602175462/login/page.jsx
+++ src/app/assingment/10/2602175462/login/page.jsx
@@ -1,8 +1,13 @@
 'use client'
 import React from 'react'
 import { useRouter } from 'next/navigation'
+import styles from "./page.module.css"
+import Link from 'next/link'
 
 export default function LoginPage() {
+
+
+    
     const router = useRouter();
 
     function goToRegister(){
@@ -11,9 +16,19 @@ export default function LoginPage() {
 
 
   return (
-    <div style={{display:'flex'}}>
-        <h1>Login page</h1>
-        <button onClick={goToRegister}>Go to Register page</button>
-    </div>
+        <div className={styles.container}>
+            <div className={styles.mainContainer}>
+                <h1>Login Page</h1>
+                <h3 style={{margin:'0'}}>Email</h3>
+                <input type="text" name='email' className={styles.input}/>
+                <h3 style={{margin:'0'}}>Password</h3>
+                <input type="text" name='password' className={styles.input}/>
+                <h3 style={{margin:'0'}}>Confirm Password</h3>
+                <input type="text" name='confirmPassword' style={{marginBottom:'1rem'}} className={styles.input}/> <br />
+                <button className={styles.button}>Login</button>
+                <p>Don't have account ? <Link href='/assingment/10/2602175462/register'>Go to Register Page</Link></p>
+            </div>
+            {/* <button onClick={goToLogin}>Go to login</button> */}
+        </div>
   )
 }
--- src/app/assingment/10/2602175462/register/page.jsx
+++ src/app/assingment/10/2602175462/register/page.jsx
@@ -1,29 +1,118 @@
 'use client';
-import React from 'react'
+import React, { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import styles from './page.module.css'
 import Link from 'next/link';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase/Init';
+import { setDoc } from 'firebase/firestore';
+import { db } from '../firebase/Init';
+import { doc } from 'firebase/firestore';
 
 export default function RegisterPage() {
-    const router = useRouter();
-
-    function goToLogin(){
-        router.push(`/assingment/10/2602175462/login`)
-    }
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [validateErrors, setValidateErrors] = useState({
+        name:"",
+        email:"",
+        password:"",
+        confirmPassword:"",
+    })
     
+    const handleRegister = async()=>{
+        const newErrors = {
+            name:"",
+            email:"",
+            password:"",
+            confirmPassword:"",
+        }
+
+        let isValid = true;
+
+        if(name === ""){
+            newErrors.name = "This field must be filled";
+            isValid = false;
+        } else if(name.length < 6){
+            newErrors.name = "Name must be more than 6 letter"
+            isValid = false;
+        }
+
+        if(email === ""){
+            newErrors.email = "This field must be filled";
+            isValid = false;
+        } else if(!email.includes('@')){
+            newErrors.email = "This section must contain @ ex: example@gmail.com"
+            isValid = false;
+        }
+
+        if(password === ""){
+            newErrors.password = "This field must be filled";
+            isValid = false;
+        } else if(password.length < 6){
+            newErrors.password = "Password must be more than 6 letter"
+            isValid = false;
+        } else if(password !== confirmPassword){
+            newErrors.password = "Password and Confirm Password must be same";
+            isValid = false;
+        }
+
+        if(confirmPassword === ""){
+            newErrors.confirmPassword = "This field must be filled";
+            isValid = false;
+        } else if(confirmPassword.length < 6){
+            newErrors.confirmPassword = "Confirm Password must be more than 6 letters"
+            isValid = false;
+        } else if(confirmPassword !== password){
+            newErrors.confirmPassword = "Password and Confirm Password must be same"
+            isValid = false;
+        }
+
+        setValidateErrors(newErrors);
+
+        if(!isValid) return;
+
+        try{
+            const userInfo = await createUserWithEmailAndPassword(auth, email, password);
+            await setDoc(doc(db, "users", userInfo.user.uid),{name, email})
+            console.log(`firestore success`);
+            console.log(userInfo);
+            alert(`Success, now go to login and login with your account`)
+        } catch (error){
+            // can add some UI fallback
+            console.log(error);
+            alert(`Error Message: ${error}`);
+        }
+    }
+
+
+
     return (
         <div className={styles.container}>
             <div className={styles.mainContainer}>
                 <h1>Register Page</h1>
                 <h3 style={{margin:'0'}}>Name</h3>
-                <input type="text" name='name' className={styles.input} />
+                <input type="text" name='name' className={styles.input} value={name} onChange={(e)=> setName(e.target.value)}/>
+                {validateErrors.name &&(
+                    <p className={styles.errors}>{validateErrors.name}</p>
+                )}
                 <h3 style={{margin:'0'}}>Email</h3>
-                <input type="text" name='email' className={styles.input}/>
+                <input type="text" name='email' className={styles.input} value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                {validateErrors.email &&(
+                    <p className={styles.errors}>{validateErrors.email}</p>
+                )}
                 <h3 style={{margin:'0'}}>Password</h3>
-                <input type="text" name='password' className={styles.input}/>
+                <input type="text" name='password' className={styles.input} value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                {validateErrors.password &&(
+                    <p className={styles.errors}>{validateErrors.password}</p>
+                )}
                 <h3 style={{margin:'0'}}>Confirm Password</h3>
-                <input type="text" name='confirmPassword' style={{marginBottom:'1rem'}} className={styles.input}/> <br />
-                <button className={styles.button}>Login</button>
+                <input type="text" name='confirmPassword' className={styles.input} value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}/> <br />
+                {validateErrors.confirmPassword &&(
+                    <p className={styles.errors}>{validateErrors.confirmPassword}</p>
+                )}
+                <button className={styles.button} onClick={handleRegister}>Register</button>
                 <p>Already have account ? <Link href='/assingment/10/2602175462/login'>Go to Login Page</Link></p>
             </div>
             {/* <button onClick={goToLogin}>Go to login</button> */}
